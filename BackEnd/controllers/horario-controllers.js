093e1d4d14429cfccec4e8ceefabf539fcb95e61
Joseflores1087 <43646040+Joseflores1087@users.noreply.github.com>
2022-07-24 21:47:27 -0300

    Update horario-controllers.js

--- BackEnd/controllers/horario-controllers.js
+++ BackEnd/controllers/horario-controllers.js
@@ -6,17 +6,17 @@ const pool = require("../database/database");
 const GetHorario = async (req, res = response)=>{
     const {id} = req.params;
     console.log('Hola', id);
-try {
-    horario = await pool.query("SELECT * FROM horarios WHERE id_cancha = ?",[id]);
+// try {
+//     horario = await pool.query("SELECT * FROM horarios WHERE id_cancha = ?",[id]);
     
-        if (turno.length > 0) {
-            return res.send(horario);
-        } else {
-            res.status(404).json({ message: 'Not result' });
-        }
-} catch (error) {
+//         if (turno.length > 0) {
+//             return res.send(horario);
+//         } else {
+//             res.status(404).json({ message: 'Not result' });
+//         }
+// } catch (error) {
     
-}
+// }
 }
 /****************END GET TURNO***********************/
 
