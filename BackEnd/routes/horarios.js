2d014a5013ff1f805684f57bc67f62727f984e9c
Joseflores1087 <43646040+Joseflores1087@users.noreply.github.com>
2022-07-24 21:51:25 -0300

    back

--- BackEnd/routes/horarios.js
+++ BackEnd/routes/horarios.js
@@ -4,6 +4,6 @@ const {  GetHorario } = require ('../controllers/horario-controllers');
 
 const router = Router ();
 
-router.get('/GetHorario/:id',GetHorario);
+router.get('/GetHorario/:id',GetHorario)
 
 module.exports = router;
